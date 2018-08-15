b1e64815ccc42cb8506381e80f35b2a977d9d3c6
Bojan-Lukic-994 <boka.lukic.994@gmail.com>
2018-08-15 15:59:31 +0200

    Added changes to Hello World project.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,18 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        int myFirstNumber = 15 + 5;
+        int mySecondNumber = 10;
+        int myThirdNumber = mySecondNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastNumber = 1000 - myTotal;
+
+        System.out.println(myLastNumber);
+
+
     }
 }
