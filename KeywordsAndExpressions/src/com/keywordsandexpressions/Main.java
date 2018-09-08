cac6b87556f6b28942968865c19f36c28a28ad38
Bojan-Lukic-994 <boka.lukic.994@gmail.com>
2018-09-08 10:14:32 +0200

    Started new section - Expressions, Statements, Code blocks, Methods

---
+++ KeywordsAndExpressions/src/com/keywordsandexpressions/Main.java
@@ -0,0 +1,24 @@
+package com.keywordsandexpressions;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //example of a keyword
+        int number1 = 5;
+        int number2 = 10;
+
+        //one mile = 1.609344 km
+        //example of an expression
+        double km = (100 * 1.609344);
+
+        int number3 = 40;
+        if (number3 == 30) {
+            System.out.println("Number3 is equal to 30");
+        }
+
+
+
+
+
+    }
+}
